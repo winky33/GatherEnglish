2b814b1e2a26bac8be1c62b128b68038fc3f7ea9
Winky33 <wingshan90@yahoo.com>
2022-04-11 22:29:34 +0800

    Edited card view

--- app/src/main/java/com/example/gatherenglsih/CardView.java
+++ app/src/main/java/com/example/gatherenglsih/CardView.java
@@ -3,9 +3,7 @@ package com.example.gatherenglsih;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
@@ -13,8 +11,6 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -26,12 +22,10 @@ public class CardView extends AppCompatActivity {
     //widgets
     ImageView cardDiagram, exitBtn;
     ImageButton playAudio, upgradeCard, swapDiagram;
-    TextView coinTxt, cardTitleTxt;
+    TextView coinTxt, cardTitleTxt, cardViewTxt;
 
     //vars
-    public int coinAmount;
-    public int selectedImage;
-    public int currentCardId;
+    public int coinAmount, selectedImage, cardID,currentCardId;
     public String uuid;
 
     @Override
@@ -45,6 +39,7 @@ public class CardView extends AppCompatActivity {
         upgradeCard = findViewById(R.id.upgrade_btn);
         exitBtn = findViewById(R.id.cardViewExitBtn);
         coinTxt = findViewById(R.id.cardView_coinText);
+        cardViewTxt = findViewById(R.id.cardViewText);
         swapDiagram = findViewById(R.id.swap_btn);
 
         exitBtn.setOnClickListener(view -> startActivity(new Intent(CardView.this, CardGallery.class)));
@@ -59,26 +54,22 @@ public class CardView extends AppCompatActivity {
             coinTxt.setText(String.valueOf(coinAmount));
             cardDiagram.setImageResource(selectedImage);
         }
-        int cardID = db.getCardIDbyDiagram(String.valueOf(selectedImage));
+        cardID = db.getCardIDbyDiagram(String.valueOf(selectedImage));
 
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                if(i!=TextToSpeech.ERROR){//if no error found
-                    // To Choose language of speech
-                    textToSpeech.setLanguage(Locale.US);
-                }
+        textToSpeech = new TextToSpeech(getApplicationContext(), i -> {
+            if(i!=TextToSpeech.ERROR){//if no error found
+                // To Choose language of speech
+                textToSpeech.setLanguage(Locale.US);
             }
         });
 
         //Set Card Title
         String cardTitle = db.getCardTitle(cardID);
         cardTitleTxt.setText(cardTitle);
+        cardViewTxt.setText(cardTitle);
 
         //Play Audio Function
-        playAudio.setOnClickListener(view -> {
-            textToSpeech.speak(cardTitle,TextToSpeech.QUEUE_FLUSH,null);
-        });
+        playAudio.setOnClickListener(view -> textToSpeech.speak(cardTitle,TextToSpeech.QUEUE_FLUSH,null));
 
         //Upgrade Card Function
         if(cardID % 2 != 0){//if it is a LV1 card
@@ -87,34 +78,12 @@ public class CardView extends AppCompatActivity {
             if(!availability){//if the LV2 card info is not stored in usercard table
                 upgradeCard.setVisibility(View.VISIBLE);
 
-                upgradeCard.setOnClickListener(view -> {
-                    if (coinAmount >= 5){
-                        String currentTime = Calendar.getInstance().getTime().toString();
-                        coinAmount = coinAmount- 5;
-
-                        db.updateCoinAmount(uuid, coinAmount);
-
-                        boolean upgrade = db.storeUpgradeCard(cardID, currentTime);
-                        if (upgrade){
-                            upgradeCard.setVisibility(View.GONE);
-                            cardDiagram.setImageResource(db.getDiagram(cardID+1));
-                            coinTxt.setText(String.valueOf(coinAmount));
-                        }else{
-                            Toast errorToast = Toast.makeText(CardView.this, "Error", Toast.LENGTH_SHORT);
-                            errorToast.show();
-                        }
-                    }else{
-                        new AlertDialog.Builder(CardView.this)
-                                .setTitle("Alert")
-                                .setMessage("Coin Amount Insufficient, Please collect it by completing Exercises.")
-                                .setCancelable(true).setPositiveButton("Go to Exercise", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                startActivity(new Intent(CardView.this,Exercises.class));
-                            }
-                        }).show();
-                    }
-                });
+                upgradeCard.setOnClickListener(view -> new AlertDialog.Builder(CardView.this)
+                        .setTitle("Alert")
+                        .setMessage("Are you sure to use 5 coins to upgrade this card?")
+                        .setCancelable(true).setPositiveButton("Yes", (dialogInterface, i) -> upgradeCard())
+                        .setNegativeButton("cancel", (dialogInterface, i) -> dialogInterface.dismiss())
+                        .show());
             } else {
                 upgradeCard.setVisibility(View.GONE);
             }
@@ -139,4 +108,31 @@ public class CardView extends AppCompatActivity {
         });
 
     }
+
+    public void upgradeCard(){
+        if (coinAmount >= 5){
+            String currentTime = Calendar.getInstance().getTime().toString();
+            coinAmount = coinAmount- 5;
+
+            db.updateCoinAmount(uuid, coinAmount);
+
+            boolean upgrade = db.storeUpgradeCard(cardID, currentTime);
+            if (upgrade){
+                upgradeCard.setVisibility(View.GONE);
+                cardDiagram.setImageResource(db.getDiagram(cardID+1));
+                coinTxt.setText(String.valueOf(coinAmount));
+            }else{
+                Toast errorToast = Toast.makeText(CardView.this, "Error", Toast.LENGTH_SHORT);
+                errorToast.show();
+            }
+        }else{
+            new AlertDialog.Builder(CardView.this)
+                    .setTitle("Alert")
+                    .setMessage("Coin Amount Insufficient, Please collect it by completing Exercises.")
+                    .setCancelable(true)
+                    .setPositiveButton("Go to Exercise", (dialogInterface, i) -> startActivity(new Intent(CardView.this,Exercises.class)))
+                    .show();
+        }
+    }
 }
+
